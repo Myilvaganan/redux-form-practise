5237f38912d6e391a049f773b64c17789639058f
Myilvaganan Sakthivel <65181847+Myilvaganan@users.noreply.github.com>
2021-10-19 08:44:41 +0530

    add new functions

--- src/validate.js
+++ src/validate.js
@@ -1,66 +1,53 @@
-const isPositiveFloat = (value) =>
+const isPositiveFloatRegex = (value) =>
   new RegExp(/^([0-9]+(\.[0-9]+)?|\.[0-9]+)$/).test(value);
+
+const isFloat = (value) =>
+  value && !(!isNaN(parseFloat(value)) && isFinite(value))
+    ? "Only float " + "values are allowed."
+    : undefined;
+const isPositiveFloat = (value) =>
+  !(isFloat(value) === undefined && parseFloat(value) >= 0)
+    ? "Only positive" + " float values are allowed."
+    : undefined;
 
 const validate = (values) => {
   const errors = {};
-
-  // if (!values.fundingPartnerDetails) {
-  //   errors.spendConfig = "Share percentage is required";
-  // } else {
   errors.spendConfig = sharePercentageValidation(values.fundingPartnerDetails);
-  // }
-
-  /* ========================================== */
-
-  if (!values.firstName) {
-    errors.firstName = "Required";
-  }
-  if (!values.lastName) {
-    errors.lastName = "Required";
-  }
-  if (!values.email) {
-    errors.email = "Required";
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = "Invalid email address";
-  }
-  if (!values.sex) {
-    errors.sex = "Required";
-  }
-  if (!values.favoriteColor) {
-    errors.favoriteColor = "Required";
-  }
-
   return errors;
 };
 
+const required = (value) => (value ? undefined : "Required");
+
 const sharePercentageValidation = (values) => {
-  let isPositiveFloatValue = true;
-  let isShareValue = false;
+  let isSharePercentage = false;
+
+  const sharePercentageValues =
+    values &&
+    values
+      .filter(({ share }) => !required(share) && share)
+      .map(({ share }) => {
+        isSharePercentage = true;
+        return share;
+      });
+
+  const sharePercentageFloatValues =
+    sharePercentageValues &&
+    sharePercentageValues
+      .map((value) => isPositiveFloat(value) && parseFloat(value))
+      .filter((val) => val);
 
   const sumOfTotalSharePercentage =
-    values &&
-    Object.values(values)
-      .map((inputValue) => {
-        if (inputValue.share) {
-          isShareValue = true;
-        }
-        return inputValue.share;
-      })
-      .map((sharePercentageValue) => {
-        if (isPositiveFloat(sharePercentageValue)) {
-          return sharePercentageValue;
-        } else {
-          isPositiveFloatValue = false;
-          return 0;
-        }
-      })
-      .reduce((acc, cur) => parseFloat(acc) + parseFloat(cur), 0);
-  if (!values || !isShareValue) {
-    return "Share percentage is required";
-  } else if (!isPositiveFloatValue) {
+    sharePercentageFloatValues &&
+    sharePercentageFloatValues.reduce((acc, cur) => acc + cur, 0);
+
+  if (!isSharePercentage) {
+    return "Share percentage is required.";
+  } else if (
+    sharePercentageValues.length !== sharePercentageFloatValues.length
+  ) {
     return "Only positive number values are allowed";
-  } else if (sumOfTotalSharePercentage !== 100) {
-    return "The sum of shares should be 100";
+  } else if (sharePercentageValues && sumOfTotalSharePercentage !== 100) {
+    return "The sum of shares should be 100.";
   }
 };
 
